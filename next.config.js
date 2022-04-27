fb39bba7ca05271e1d6d1f684412273163212351
sebasje2010 <sebastian.jativa@hotmail.com>
2022-04-27 17:39:48 -0500

    agregada confirguracion pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
-const nextConfig = {
+const withPWA=require('next-pwa');
+const runtimeCaching=require('next-pwa/cache');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register:true,
+    mode: 'production',
+    disable: false,
+    runtimeCaching,
+  },
   reactStrictMode: true,
   images: {
-    domains: ['placeimg.com','api.lorem.space','cdn.lorem.space'],
-  },
-}
-
-module.exports = nextConfig
+  domains: ['placeimg.com','api.lorem.space','cdn.lorem.space'],}
+})
